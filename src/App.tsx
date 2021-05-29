15b678525cc2f95e7fc87566ed453ced447d0134
青山 直樹 <naoyama@digitalvision.co.jp>
2021-05-29 12:54:46 +0900

    firebase init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "./firebase";
 
-type User = {
+type SkillMap = {
   id: string;
   name: string;
-  height: number;
 };
 
 function App() {
-  const [users, setUsers] = useState<User[]>([
-    { id: "ID1", name: "ゾンビ", height: 195 },
-    { id: "ID2", name: "スケルトン", height: 199 },
-    { id: "ID3", name: "クリーパー", height: 170 },
-  ]);
+  // const [users, setUsers] = useState<User[]>([
+  //   { id: "ID1", name: "ゾンビ", height: 195 },
+  //   { id: "ID2", name: "スケルトン", height: 199 },
+  //   { id: "ID3", name: "クリーパー", height: 170 },
+  // ]);
+
+  const [users, setUsers] = useState<SkillMap[]>([]);
+
+  // === 追記分 ===
+  useEffect(() => {
+    // 取得結果をコンソールに出力
+    const usersRef = db.collection("maps");
+    usersRef.get().then((snapshot) => {
+      let userList:SkillMap[] = [];
+      snapshot.forEach((doc) => {
+        console.log(doc.id, doc.data())
+        userList.push({
+          id: doc.id,
+          name: doc.data().name
+        })
+      });
+      setUsers(userList);
+    });
+  }, []);
+  // =============
+
   return (
     <div>
       <h2>Users</h2>
       {users.map((user) => (
         <div key={user.id}>
-          {user.name} ({user.height}cm)
+          {user.name}
         </div>
       ))}
     </div>
